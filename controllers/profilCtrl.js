dbcd69e91ac64c2159e4866676c35717592fc09d
dcirlig <dcirlig@student.42.fr>
2018-11-28 13:18:07 +0100

    profil ctrl d

--- controllers/profilCtrl.js
+++ controllers/profilCtrl.js
@@ -7,23 +7,53 @@ module.exports = {
   userProfil: function(req, res) {
     if (req.params.username) {
       var username = escapeHtml(req.params.username);
-      models.findOne("username", username, function(find) {
-        if (find) {
-          models.getUser("username", username, function(result) {
-            if (result) {
-              result.forEach(element => {
-                var data = {
-                  username: element.username,
-                  gender: element.gender
-                };
-                return res.status(200).json({ userData: data });
-              });
-            }
-          });
-        } else {
-          return res.json({ error: "User does not exist" });
-        }
-      });
+      var userData = {
+        localisation: req.body.localisation,
+        bio: req.body.bio,
+        gender: req.body.gender,
+        sexual_orientation: req.body.sexual_orientation,
+        interests: req.body.interests,
+        profil_image: req.body.profil_image
+      };
+
+      if (
+        userData.location !== "" &&
+        userData.gender !== "" &&
+        (userData.gender === "female" || userData.gender === "male") &&
+        !userData.profil_image &&
+        userData.bio.length <= 500 &&
+        userData.sexual_orientation !== "" &&
+        userData.interests !== ""
+      ) {
+        models.findOne("username", username, function(find) {
+          if (find) {
+            models.getUser("username", username, function(result) {
+              if (result) {
+                result.forEach(element => {
+                  models.updateUser(
+                    `localisation='${userData.localisation}', bio='${
+                      userData.bio
+                    }', gender = '${userData.gender}', sexual_orientation='${
+                      userData.sexual_orientation
+                    }', profil_image='${userData.profil_image}'`,
+                    element.userId
+                  );
+                  return res.status(200).json({
+                    success: "Successfully completed"
+                  });
+                });
+              } else {
+                res.json({ error: "user does not exists" });
+              }
+            });
+          } else {
+            return res.json({ error: "User does not exist" });
+          }
+        });
+      } else {
+        res.json({ error: "Please complete the required fields" });
+      }
     }
+  }
   }
 };
--- database/database.js
+++ database/database.js
@@ -23,28 +23,69 @@ con.connect(function(err) {
     con.connect(function(err) {
       if (err) console.log(err);
       console.log("Connected to database!");
+      sql = "DROP TABLE IF EXISTS `images`";
+      con.query(sql, function(err, result) {
+        if (err) console.log(err);
+        console.log("Delete images table");
+      });
+      sql = "DROP TABLE IF EXISTS `interests`";
+      con.query(sql, function(err, result) {
+        if (err) console.log(err);
+        console.log("Delete interests table");
+      });
       sql = "DROP TABLE IF EXISTS `users`";
       con.query(sql, function(err, result) {
         if (err) console.log(err);
         console.log("Delete users table");
       });
+
       var sql = `CREATE TABLE if NOT EXISTS users
     (
-    userId            INTEGER AUTO_INCREMENT PRIMARY KEY,
-    firstname         VARCHAR(45) NOT NULL,
-    lastname          VARCHAR(45) NOT NULL,
-    email             VARCHAR(45) NOT NULL,
-    username          VARCHAR(45) NOT NULL,
-    passwd            VARCHAR(255) NOT NULL,
-    emailVerified     VARCHAR(45) DEFAULT false,
-    secretTokenEmail  VARCHAR(255) NOT NULL,
-    gender     VARCHAR(45) DEFAULT 'female',
-    resetPasswordToken VARCHAR(255) DEFAULT NULL
+    userId              INTEGER AUTO_INCREMENT PRIMARY KEY,
+    firstname           VARCHAR(45) NOT NULL,
+    lastname            VARCHAR(45) NOT NULL,
+    email               VARCHAR(45) NOT NULL,
+    username            VARCHAR(45) NOT NULL,
+    passwd              VARCHAR(255) NOT NULL,
+    emailVerified       VARCHAR(45) DEFAULT false,
+    secretTokenEmail    VARCHAR(255) NOT NULL,
+    gender              VARCHAR(45) DEFAULT 'female',
+    resetPasswordToken  VARCHAR(255) DEFAULT NULL,
+    bio                 VARCHAR(255) DEFAULT '',
+    localisation        VARCHAR(255) DEFAULT '',
+    sexual_orientation  VARCHAR(255) DEFAULT 'bisexual',
+    profil_image        VARCHAR(255) DEFAULT NULL
     )`;
 
       con.query(sql, function(err, result) {
         if (err) console.log(err);
         console.log("Create table users");
+      });
+
+      var sql = `CREATE TABLE if NOT EXISTS images
+    (
+    imageId            INTEGER AUTO_INCREMENT PRIMARY KEY,
+    url                VARCHAR(45) NOT NULL,
+    userId             INTEGER NOT NULL,
+    FOREIGN KEY (userId) REFERENCES users(userId)
+    )`;
+
+      con.query(sql, function(err, result) {
+        if (err) console.log(err);
+        console.log("Create table interests");
+      });
+
+      var sql = `CREATE TABLE if NOT EXISTS interests
+      (
+      interestId         INTEGER AUTO_INCREMENT PRIMARY KEY,
+      content            VARCHAR(45) NOT NULL,
+      userId             INTEGER NOT NULL,
+      FOREIGN KEY (userId) REFERENCES users(userId)
+      )`;
+
+      con.query(sql, function(err, result) {
+        if (err) console.log(err);
+        console.log("Create table interests");
       });
     });
   });
